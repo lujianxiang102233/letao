66797fa130eb2cc0010ac7069333e0817658ffc4
lujianxiang <409627448@qq.com>
2018-08-07 19:25:28 +0800

    登录验证功能完善

--- public/back/js/index.js
+++ public/back/js/index.js
@@ -2,43 +2,48 @@
 
 //登录功能
 $(function(){
-   
-    $('#form').bootstrapValidator({
-        //2. 指定校验时的图标显示，默认是bootstrap风格
-        feedbackIcons: {
-          valid: 'glyphicon glyphicon-ok',
-          invalid: 'glyphicon glyphicon-remove',
-          validating: 'glyphicon glyphicon-refresh'
-        },
-      
-        //3. 指定校验字段
-        fields: {
-        
-          username: {
-            validators:{
-                notEmpty:{
+   $('#form').bootstrapValidator({
+       //校验图标
+       feedbackIcons: {
+        valid: 'glyphicon glyphicon-ok',
+        invalid: 'glyphicon glyphicon-remove',
+        validating: 'lyphicon glyphicon-refresh'
+       },
+
+
+    fields: {
+        username: {
+            validators: {
+                //校验是否为空
+                notEmpty: {
                     message:'用户名不能为空'
                 },
-                stringLength:{
+                //校验长度
+                stringLength: {
                     min:2,
                     max:6,
-                    message:'用户名长度必须在2到6之间'
+                    message: '用户名长度必须在2到6之间'
                 }
             }
-          },
-          password:{
-              validators:{
-                  notEmpty:{
-                      message:'密码不能为空'
-                  },
-                  stringLength:{
-                      min:6,
-                      max:12,
-                      message:'密码长度必须在6到12之间'
-                  }
-              }
-          }
+        },
+        password:{
+            validators: {
+                notEmpty: {
+                    message: '密码不能为空'
+                },
+                stringLength: {
+                    min:6,
+                    max:12,
+                    message: '密码长度必须在6到12之间'
+                }
+            }
         }
-      
-      });
+    }
+   })
+
+//validator.resetForm() 重置表单
+   $('.reset').on('click', function() {
+       $('#form').data('bootstrapValidator').resetForm();
+   })
+
 })
