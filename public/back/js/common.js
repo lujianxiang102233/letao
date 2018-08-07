378547159adb33bb3eae8008367b81ed3c996c9d
lujianxiang <409627448@qq.com>
2018-08-07 22:41:13 +0800

    退出功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,8 +1,34 @@
+
+//侧边栏点击下拉伸缩
 $(function(){
     $('.list').click(function(){
         console.log(123);
         $('.in').slideToggle();
     })
+
+//菜单栏点击，侧边栏隐藏
+$('.icon-menu').click(function(){
+    $('.lt-aside').toggleClass('current');
+    $('.it-main').toggleClass('current');
+    $('.it-main .it-topbar').toggleClass('current');
+})
+
+//退出功能
+
+$('.out').click(function(){
+    console.log(1234);
+    $.ajax({
+        type:'get',
+        url:'/employee/employeeLogout',
+        dataType:'json',
+        success:function(info){
+            //console.log(info);
+            if(info.success){
+                location.href = "login.html";
+            }
+        }
+    })
+})
 
 
 
