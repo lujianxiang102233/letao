300822de9de58bf4b027d9a7fd8eb1dbb28fc833
lujianxiang <409627448@qq.com>
2018-08-07 14:25:08 +0800

    登录功能完成

--- public/back/js/index.js
+++ public/back/js/index.js
@@ -1,3 +1,44 @@
 
 
 //登录功能
+$(function(){
+   
+    $('#form').bootstrapValidator({
+        //2. 指定校验时的图标显示，默认是bootstrap风格
+        feedbackIcons: {
+          valid: 'glyphicon glyphicon-ok',
+          invalid: 'glyphicon glyphicon-remove',
+          validating: 'glyphicon glyphicon-refresh'
+        },
+      
+        //3. 指定校验字段
+        fields: {
+        
+          username: {
+            validators:{
+                notEmpty:{
+                    message:'用户名不能为空'
+                },
+                stringLength:{
+                    min:2,
+                    max:6,
+                    message:'用户名长度必须在2到6之间'
+                }
+            }
+          },
+          password:{
+              validators:{
+                  notEmpty:{
+                      message:'密码不能为空'
+                  },
+                  stringLength:{
+                      min:6,
+                      max:12,
+                      message:'密码长度必须在6到12之间'
+                  }
+              }
+          }
+        }
+      
+      });
+})
