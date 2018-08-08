97eb27d0b88f967aa3156a55aed4944ec53dc735
lujianxiang <409627448@qq.com>
2018-08-08 14:25:56 +0800

    侧边栏二级菜单显示隐藏完成

--- public/back3/js/commit.js
+++ public/back3/js/commit.js
@@ -8,6 +8,12 @@ $(function(){
     $(document).ajaxStop(function(){
         NProgress.done();
     })
+
+
+    //侧边栏二级菜单显示和隐藏
+    $('.aside-nav .list').click(function(){
+        $('.aside-nav .next-list').slideToggle();
+    })
 })
 
 
