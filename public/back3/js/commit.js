e70f52b940e5ca1bbe9bfd72878810e00f7464f1
lujianxiang <409627448@qq.com>
2018-08-08 12:19:08 +0800

    增加进度条功能

---
+++ public/back3/js/commit.js
@@ -0,0 +1,11 @@
+$(function(){
+    //增加进度条
+
+    $(document).ajaxStart(function(){
+        NProgress.start();
+    })
+
+    $(document).ajaxStop(function(){
+        NProgress.done();
+    })
+})
