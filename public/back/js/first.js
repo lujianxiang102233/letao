366efbd4523b2163b3ea47b090d8f64f9803e2c6
lujianxiang <409627448@qq.com>
2018-08-09 23:17:54 +0800

    分类管理的以及分类功能完善

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -67,47 +67,25 @@ $(function () {
         }
     })
 
+    $("#addForm").on('success.form.bv', function (e) {
+        e.preventDefault();
+        //使用ajax提交逻辑
+       $.ajax({
+           type: 'post',
+           url: '/category/addTopCategory',
+           data: $('#addForm').serialize(),
+           dataType:'json',
+           success: function(info) {
+               console.log(info);
+               if( info.success ){
+                   $('#addModal').modal('hide');
+                   currentPage = 1;
+                   render();
+                   $('#addForm').data("bootstrapValidator").resetForm(true);
+               }
+           }
+       })
+    });
 
-    // $("#addForm").on('success.form.bv', function (e) {
-    //     e.preventDefault();
-    //     //使用ajax提交逻辑
-    // });
-    // $("#addForm").bootstrapValidator({
-    //     //1. 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)'],可以不设置
-    //     excluded: [':disabled', ':hidden', ':not(:visible)'],
-
-    //     //2. 指定校验时的图标显示，默认是bootstrap风格
-    //     feedbackIcons: {
-    //       valid: 'glyphicon glyphicon-ok',
-    //       invalid: 'glyphicon glyphicon-remove',
-    //       validating: 'glyphicon glyphicon-refresh'
-    //     },
-
-    //     //3. 指定校验字段
-    //     fields: {
-    //       //校验用户名，对应name表单的name属性
-    //       username: {
-    //         validators: {
-    //           //不能为空
-    //           notEmpty: {
-    //             message: '用户名不能为空'
-    //           },
-    //           //长度校验
-    //           stringLength: {
-    //             min: 6,
-    //             max: 30,
-    //             message: '用户名长度必须在6到30之间'
-    //           },
-    //           //正则校验
-    //           regexp: {
-    //             regexp: /^[a-zA-Z0-9_\.]+$/,
-    //             message: '用户名由数字字母下划线和.组成'
-    //           }
-    //         }
-    //       },
-    //     }
-
-
-    // })
 
 })
