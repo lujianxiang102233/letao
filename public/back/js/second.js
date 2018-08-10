cbd9ec05c9f5a9184ce8f2282baa9716b24bf4db
lujianxiang <409627448@qq.com>
2018-08-10 20:12:01 +0800

    二级分类功能完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -12,7 +12,7 @@ $(function(){
             },
             dataType: 'json',
             success: function (info) {
-                console.log(info);
+                //console.log(info);
                 //把第一页数据线渲染出来
                 var htmlStr = template('tmp', info);
                 $('tbody').html(htmlStr);
@@ -38,5 +38,118 @@ $(function(){
 
     $('.addBtn').click(function(){
         $('#addModal').modal('show');
+        //ul里面的li的数据渲染
+        $.ajax({
+            type:'get',
+            url: '/category/queryTopCategoryPaging',
+            data: {
+                page:1,
+                pageSize:100
+            },
+            dataType: 'json',
+            success: function( info ){
+                //console.log( info );
+                var liStr = template('dropdownTpl',info);
+                $('.dropdown-menu').html(liStr);
+
+            }
+
+        })
     })
+
+
+    //通过实践委托，给li里面所有的a标签添加事件
+    $('.dropdown-menu').on('click','a',function(){
+        var txt = $(this).text();
+        $('.categoryName').text(txt);
+
+        var id = $(this).data('id');
+        $('[name="categoryId"]').val(id);
+        $('#addForm').data('bootstrapValidator').updateStatus("categoryId", "VALID");
+    })
+
+
+    $("#fileupload").fileupload({
+        dataType:"json",
+        //e：事件对象
+        //data：图片上传后的对象，通过data.result.picAddr可以获取上传后的图片地址
+        done:function (e, data) {
+          console.log(data.result.picAddr);
+          var url = data.result.picAddr;
+          $('.uploadImg').attr('src',url);
+          $('[name="brandLogo"]').val(url); 
+          $('#addForm').data('bootstrapValidator').updateStatus("brandLogo", "VALID");
+        }
+  });
+
+
+
+
+  //表单验证
+  $('#addForm').bootstrapValidator({
+        excluded: [],
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+          },
+       //3. 指定校验字段
+        fields: {
+          //校验用户名，对应name表单的name属性
+          categoryId: {
+            validators: {
+              //不能为空
+              notEmpty: {
+                message: '请选择一级分类'
+              },
+            }
+          },
+          brandName: {
+            validators: {
+                //不能为空
+                notEmpty: {
+                  message: '请选择二级分类'
+                },
+              }
+          },
+          brandLogo: {
+            validators: {
+                //不能为空
+                notEmpty: {
+                  message: '请选择图片'
+                }
+              }
+          }
+        }
+    
+  })
+
+
+
+
+  $("#addForm").on('success.form.bv', function (e) {
+    e.preventDefault();
+    //使用ajax提交逻辑
+    $.ajax({
+        type: 'post',
+        url: '/category/addSecondCategory',
+        data: $('#addForm').serialize(),
+        dataType: 'json',
+        success: function( info ) {
+            //console.log(info);
+            if ( info.success){
+                $('#addModal').modal('hide');
+                currentPage = 1;
+                render();
+                $('#addForm').data('bootstrapValidator').resetForm(true);
+                $('.categoryName').text('请选择一级分类');
+                $('.uploadImg').attr('src','images/none.png');
+            }
+        }
+    })
+});
+
+    
+
+
 })
