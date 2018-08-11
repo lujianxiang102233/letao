aaf34c738cc486c19b3b15bf586b6f51e303c1a1
lujianxiang <409627448@qq.com>
2018-08-11 10:45:04 +0800

    商品管理功能完成

--- public/back/js/product.js
+++ public/back/js/product.js
@@ -3,6 +3,10 @@
 $(function(){
     var currentPage = 1;
     var pageSize = 5;
+    var picArr = [];
+
+
+
     render();
  function render() {
     $.ajax({
@@ -125,6 +129,10 @@ $(function(){
             //不能为空
             notEmpty: {
               message: '请输入商品库存'
+            },
+            regexp: {
+              regexp: /^[1-9]\d*$/,
+              message: '商品库存必须是非零开头的数字'
             }
           } 
       },
@@ -133,6 +141,10 @@ $(function(){
             //不能为空
             notEmpty: {
               message: '请输入商品尺码'
+            },
+            regexp: {
+              regexp: /^\d{2}-\d{2}$/,
+              message: '商品尺码必须是xx-xx的格式，例如，32-40'
             }
           } 
       },
@@ -165,6 +177,62 @@ $(function(){
 
 })
 
+
+
+//图片上传，获取上传后图片的地址$("#fileupload").fileupload({
+  $("#fileupload").fileupload({
+    dataType:"json",
+    //e：事件对象
+    //data：图片上传后的对象，通过data.result.picAddr可以获取上传后的图片地址
+    done:function (e, data) {
+      console.log(data.result);
+      picArr.unshift(data.result);
+      //console.log(picArr);
+
+      var picUrl = data.result.picAddr;
+      $('#imgBox').prepend('<img src="' + picUrl+  ' " width="100">');
+
+      if(picArr.length > 3) {
+        picArr.pop();
+        $('#imgBox img:last-of-type').remove();
+      }
+
+      if( picArr.length === 3){
+        $('#addForm').data("bootstrapValidator").updateStatus("picStatus", "VALID");
+      }
+
+
+    }
+});
+
+//表单验证成功
+
+$("#addForm").on('success.form.bv', function (e) {
+  e.preventDefault();
+  //使用ajax提交逻辑
+  var paramstar = $('#addForm').serialize();
+  paramstar +=  "&picAddr1=" + picArr[0].picAddr + "&picName1=" + picArr[0].picName;
+  paramstar +=  "&picAddr2=" + picArr[1].picAddr + "&picName2=" + picArr[1].picName;
+  paramstar +=  "&picAddr3=" + picArr[2].picAddr + "&picName3=" + picArr[2].picName;
+  $.ajax({
+    type: 'post',
+    url: '/product/addProduct',
+    data: paramstar,
+    dataType: 'json',
+    success: function (info) {
+      //console.log(info);
+      if( info.success){
+        $('#addModal').modal('hide');
+        $('#imgBox img').remove();
+        picArr = [];
+        currentPage = 1;
+        render();
+        $("#addForm").data('bootstrapValidator').resetForm(true);
+        $('#dropdownText').text('请选择二级分类');
+      }
+    }
+  })
+});
 
 
 
